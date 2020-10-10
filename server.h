8a0bd4de98bbc48ef40f2042c7842a5ef80a35a8
liqi17-thu <624275030@qq.com>
2020-10-10 14:38:41 +0800

    minor

--- server.h
+++ server.h
@@ -1,15 +1,14 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-#include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 
 /*-------------------------------MARCO------------------------------------*/
 #define DEFAULT_PORT 21
 #define DEFAULT_ROOT_PATH "/tmp/"
 #define BACKLOG 5
 #define BUF_SIZE 8192
-
 
 /*-------------------------------STRUCT-----------------------------------*/
 typedef struct Server {
@@ -18,13 +17,12 @@ typedef struct Server {
     char root_path[100];
     struct sockaddr_in addr;
     socklen_t addrlen;
-    int control_sockfd;      
+    int control_sockfd;
 } Server;
-
 
 /*------------------------------FUNCTIONS---------------------------------*/
 // setup server
-int setup_server(int argc, char ** argv);
-int setup_root_and_port(int argc, char ** argv);
+int setup_server(int argc, char **argv);
+int setup_root_and_port(int argc, char **argv);
 
 #endif
--- utils.c
+++
@@ -1,5 +0,0 @@
-
-
-int setup_server(int argc, char ** argv) {
-	
-}
